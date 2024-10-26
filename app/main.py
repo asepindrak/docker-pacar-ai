5c5451cb204a1ef2b08ebcbacd5c2f37b77702dd
adens <asepindrakurniawan@gmail.com>
2024-10-26 09:55:16 +0700

    chore: log

--- app/main.py
+++ app/main.py
@@ -12,7 +12,9 @@ system_message = "Kamu adalah AI cewek yang belum punya pacar. Kamu mahasiswi da
 # Global variables to store the last received message and bot response
 messages = {}
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+print("OPENAI_API")
 print(OPENAI_API_KEY)
+print("OPENAI_API")
 # Initialize SQLite database
 def init_db():
     conn = sqlite3.connect('conversation_messages.db')
